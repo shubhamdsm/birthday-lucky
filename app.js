06d4331112919bb1fca43306ce220462735e5f11
shubham <33717931+shubhamdsm@users.noreply.github.com>
2021-01-04 19:39:54 +0530

    functionality added

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
 let userDate = document.querySelector('#date');
 let userMonth = document.querySelector('#month');
 let userYear = document.querySelector('#year');
+let userLuckyNumber = document.querySelector('#lucky');
+let btn = document.querySelector("#btn")
 
+
+
+function clickHandler(){
+    let addBirth = userDate+userMonth+userYear;
+    if(addBirth % userLuckyNumber===0){
+        document.querySelector('#print').innerText = 'your birthday is lucky'
+    }else{
+        document.querySelector('#print').innerText = 'sorry your birthday is not lucky'
+    }
+    
+}
+
+btn.addEventListener('click',clickHandler)
